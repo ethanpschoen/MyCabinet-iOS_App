50446282e3f4ce568ee8ad8481fcfe02e06f33f5
Ethan Schoen <ethanparkerschoen@gmail.com>
2024-08-15 16:25:18 -0700

    Commit test

--- MyCabinet/Source/Models/File.swift
+++ MyCabinet/Source/Models/File.swift
@@ -1,8 +1,42 @@
-//
-//  File.swift
-//  MyCabinet
-//
-//  Created by Ethan on 8/15/24.
-//
-
 import Foundation
+
+// MARK: - Item Model
+
+struct Item: Identifiable, Codable {
+    // Unique identifier for each item
+    var id: UUID
+    
+    // The name of the item
+    var name: String
+    
+    // The date the item was added
+    var dateAdded: Date
+    
+    // The expiration date of the item
+    var expirationDate: Date
+    
+    // Optional notes for the item
+    var notes: String?
+    
+    // Computed property to check if the item is expired
+    var isExpired: Bool {
+        return expirationDate < Date()
+    }
+    
+    // Computed property to calculate days until expiration
+    var daysUntilExpiration: Int? {
+        let calendar = Calendar.current
+        return calendar.dateComponents([.day], from: Date(), to: expirationDate).day
+    }
+    
+    // Initializer
+    init(name: String, dateAdded: Date = Date(), expirationDate: Date, notes: String? = nil) {
+        self.id = UUID()
+        self.name = name
+        self.dateAdded = dateAdded
+        self.expirationDate = expirationDate
+        self.notes = notes
+    }
+}
+
+
