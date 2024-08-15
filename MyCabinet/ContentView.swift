1402d5cca273e7af7600d7d3454323fdcadac54c
Ethan Schoen <ethanparkerschoen@gmail.com>
2024-08-15 16:10:49 -0700

    Initial Commit

---
+++ MyCabinet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCabinet
+//
+//  Created by Ethan on 8/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyCabinet/MyCabinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCabinetApp.swift
+//  MyCabinet
+//
+//  Created by Ethan on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCabinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
