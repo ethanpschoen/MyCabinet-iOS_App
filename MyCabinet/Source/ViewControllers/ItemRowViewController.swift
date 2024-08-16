f9be4d63c661d2876e3ea024901bc666563e7ff7
Ethan Schoen <ethanparkerschoen@gmail.com>
2024-08-16 13:10:18 -0700

    Update notifications and view controller

---
+++ MyCabinet/Source/ViewControllers/ItemRowViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRowViewController.swift
+//  MyCabinet
+//
+//  Created by Ethan on 8/16/24.
+//
+
+import SwiftUI
+
+struct ItemRowViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRowViewController()
+}
